bec0a8a94dd212510ab5957102caaebbcad31300
CEEPREL <ceeprel3@gmail.com>
2025-04-05 18:28:41 +0100

    new msg commit

--- app/components/welcome-toast.tsx
+++ app/components/welcome-toast.tsx
@@ -6,21 +6,14 @@ import { toast } from 'sonner';
 export function WelcomeToast() {
   useEffect(() => {
     if (!document.cookie.includes('email-toast=1')) {
-      toast('📩 Welcome to Next.js Emails!', {
+      toast('📩 Welcome to DayLight Emails!', {
         duration: Infinity,
         onDismiss: () =>
           (document.cookie = 'email-toast=1; max-age=31536000; path=/'),
         description: (
           <p>
-            This is a demo of an email client UI with a Postgres database.{' '}
-            <a
-              href="https://vercel.com/templates/next.js/next-js-email-client"
-              className="text-blue-600 hover:underline"
-              target="_blank"
-            >
-              Deploy your own
-            </a>
-            .
+            This is a demo of an inspired email client UI with a Postgres
+            database
           </p>
         ),
       });
